582dc8f8e0273f5ca2b0255e220ae959e66b439e
pmcdowell-okta <pmcdowell@okta.com>
2018-02-03 17:54:49 -0500

    Update tinker.go

--- tinker.go
+++ tinker.go
@@ -4,11 +4,7 @@ import "fmt"
 
 func Pointlesstest() {
 
-<<<<<<< HEAD
 	fmt.Println("works, and you can update packages too !")
-=======
-	fmt.Println("works and you can update packages too")
->>>>>>> b19e80f86a9f56c66eba8d0976d048e1c126147d
 
 } 
 
